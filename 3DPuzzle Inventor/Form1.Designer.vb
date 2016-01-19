79c2accd1694918156d93da5e5145ce2657f6334
David Truyens <david.truyens@live.com>
2016-01-19 11:58:00 +0100

    RadioButtons Added

--- 3DPuzzle Inventor/Form1.Designer.vb
+++ 3DPuzzle Inventor/Form1.Designer.vb
@@ -22,9 +22,140 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Ydir = New System.Windows.Forms.RadioButton()
+        Me.Zdir = New System.Windows.Forms.RadioButton()
+        Me.Xdir = New System.Windows.Forms.RadioButton()
+        Me.SliceThickness = New System.Windows.Forms.NumericUpDown()
+        Me.NumberOfSlices = New System.Windows.Forms.NumericUpDown()
+        Me.YBox = New System.Windows.Forms.CheckBox()
+        Me.ZBox = New System.Windows.Forms.CheckBox()
+        Me.XBox = New System.Windows.Forms.CheckBox()
+        Me.GetBodyButton = New System.Windows.Forms.Button()
+        CType(Me.SliceThickness, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumberOfSlices, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Ydir
+        '
+        Me.Ydir.AutoSize = True
+        Me.Ydir.Location = New System.Drawing.Point(203, 150)
+        Me.Ydir.Name = "Ydir"
+        Me.Ydir.Size = New System.Drawing.Size(43, 17)
+        Me.Ydir.TabIndex = 14
+        Me.Ydir.Text = "Ydir"
+        Me.Ydir.UseVisualStyleBackColor = True
+        '
+        'Zdir
+        '
+        Me.Zdir.AutoSize = True
+        Me.Zdir.Location = New System.Drawing.Point(203, 173)
+        Me.Zdir.Name = "Zdir"
+        Me.Zdir.Size = New System.Drawing.Size(43, 17)
+        Me.Zdir.TabIndex = 13
+        Me.Zdir.Text = "Zdir"
+        Me.Zdir.UseVisualStyleBackColor = True
+        '
+        'Xdir
+        '
+        Me.Xdir.AutoSize = True
+        Me.Xdir.Checked = True
+        Me.Xdir.Location = New System.Drawing.Point(203, 127)
+        Me.Xdir.Name = "Xdir"
+        Me.Xdir.Size = New System.Drawing.Size(43, 17)
+        Me.Xdir.TabIndex = 12
+        Me.Xdir.TabStop = True
+        Me.Xdir.Text = "Xdir"
+        Me.Xdir.UseVisualStyleBackColor = True
+        '
+        'SliceThickness
+        '
+        Me.SliceThickness.DecimalPlaces = 2
+        Me.SliceThickness.Location = New System.Drawing.Point(38, 105)
+        Me.SliceThickness.Name = "SliceThickness"
+        Me.SliceThickness.Size = New System.Drawing.Size(120, 20)
+        Me.SliceThickness.TabIndex = 11
+        Me.SliceThickness.Value = New Decimal(New Integer() {5, 0, 0, 65536})
+        '
+        'NumberOfSlices
+        '
+        Me.NumberOfSlices.Location = New System.Drawing.Point(38, 70)
+        Me.NumberOfSlices.Name = "NumberOfSlices"
+        Me.NumberOfSlices.Size = New System.Drawing.Size(120, 20)
+        Me.NumberOfSlices.TabIndex = 10
+        Me.NumberOfSlices.Value = New Decimal(New Integer() {10, 0, 0, 0})
+        '
+        'YBox
+        '
+        Me.YBox.AutoSize = True
+        Me.YBox.Checked = True
+        Me.YBox.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.YBox.Location = New System.Drawing.Point(194, 51)
+        Me.YBox.Name = "YBox"
+        Me.YBox.Size = New System.Drawing.Size(78, 17)
+        Me.YBox.TabIndex = 18
+        Me.YBox.Text = "Y Direction"
+        Me.YBox.UseVisualStyleBackColor = True
+        '
+        'ZBox
+        '
+        Me.ZBox.AutoSize = True
+        Me.ZBox.Location = New System.Drawing.Point(194, 74)
+        Me.ZBox.Name = "ZBox"
+        Me.ZBox.Size = New System.Drawing.Size(78, 17)
+        Me.ZBox.TabIndex = 17
+        Me.ZBox.Text = "Z Direction"
+        Me.ZBox.UseVisualStyleBackColor = True
+        '
+        'XBox
+        '
+        Me.XBox.AutoSize = True
+        Me.XBox.Checked = True
+        Me.XBox.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.XBox.Location = New System.Drawing.Point(194, 25)
+        Me.XBox.Name = "XBox"
+        Me.XBox.Size = New System.Drawing.Size(78, 17)
+        Me.XBox.TabIndex = 16
+        Me.XBox.Text = "X Direction"
+        Me.XBox.UseVisualStyleBackColor = True
+        '
+        'GetBodyButton
+        '
+        Me.GetBodyButton.Location = New System.Drawing.Point(29, 21)
+        Me.GetBodyButton.Name = "GetBodyButton"
+        Me.GetBodyButton.Size = New System.Drawing.Size(75, 23)
+        Me.GetBodyButton.TabIndex = 15
+        Me.GetBodyButton.Text = "Get body"
+        Me.GetBodyButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.YBox)
+        Me.Controls.Add(Me.ZBox)
+        Me.Controls.Add(Me.XBox)
+        Me.Controls.Add(Me.GetBodyButton)
+        Me.Controls.Add(Me.Ydir)
+        Me.Controls.Add(Me.Zdir)
+        Me.Controls.Add(Me.Xdir)
+        Me.Controls.Add(Me.SliceThickness)
+        Me.Controls.Add(Me.NumberOfSlices)
+        Me.Name = "Form1"
         Me.Text = "Form1"
-    End Sub
+        CType(Me.SliceThickness, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumberOfSlices, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
+    End Sub
+    Friend WithEvents Ydir As RadioButton
+    Friend WithEvents Zdir As RadioButton
+    Friend WithEvents Xdir As RadioButton
+    Friend WithEvents SliceThickness As NumericUpDown
+    Friend WithEvents NumberOfSlices As NumericUpDown
+    Friend WithEvents YBox As CheckBox
+    Friend WithEvents ZBox As CheckBox
+    Friend WithEvents XBox As CheckBox
+    Friend WithEvents GetBodyButton As Button
 End Class
--- 3DPuzzle Inventor/Form1.vb
+++ 3DPuzzle Inventor/Form1.vb
@@ -1,3 +1,368 @@
-﻿Public Class Form1
+﻿
+Imports System
+Imports System.Type
+Imports System.Activator
+Imports System.Runtime.InteropServices
+Imports Inventor
+
+Public Class Form1
+    Public Shared _invApp As Inventor.Application
+    Dim _Doc As Inventor.PartDocument
+    Dim _started As Boolean = False
+    Dim _SplitDir As Integer
+    Dim _CompDef As ComponentDefinition
+    Dim _SecondarySlicesNumber As Integer
+    Dim _initialComp As Integer
+    Dim _PullDir As Integer = 1
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Try
+            _invApp = Marshal.GetActiveObject("Inventor.Application")
+        Catch ex As Exception
+            Try
+                Dim invAppType As Type = GetTypeFromProgID("Inventor.Application")
+                _invApp = CreateInstance(invAppType)
+                _invApp.Visible = True
+                _started = True
+                MsgBox("Inventor Started")
+
+            Catch ex2 As Exception
+                MsgBox(ex2.ToString())
+                MsgBox("unable to start Inventor")
+            End Try
+        End Try
+
+        If _invApp.Documents.Count = 0 Then
+            MsgBox("Need to open a Part Document")
+            Return
+        End If
+
+        If _invApp.ActiveDocument.DocumentType <> DocumentTypeEnum.kPartDocumentObject Then
+            MsgBox("Need to have a Part Document active")
+            Return
+        End If
+
+        _Doc = _invApp.ActiveDocument
+        _CompDef = _Doc.ComponentDefinition
+        _initialComp = _CompDef.SurfaceBodies.Count
+        If Puzzletest() Then
+            MsgBox("remove previous puzzle first")
+        End If
+
+    End Sub
+
+    Function Puzzletest() As Boolean
+
+        Dim PreviousPuzzle As Boolean = False
+        Dim Testplane As WorkPlane
+        For Each Testplane In _Doc.ComponentDefinition.WorkPlanes
+            If (Testplane.Name = "Start Puzzle") Then
+                PreviousPuzzle = True
+                Exit For
+            End If
+        Next
+        Return PreviousPuzzle
+    End Function
+
+    Private Sub GetBodyButton_Click(sender As Object, e As EventArgs) Handles GetBodyButton.Click
+        Dim body As SurfaceBody = GetBody()
+        Dim MainDir As KeyValuePair(Of Integer, Double) = GetBoundingBoxLength(body)
+        GenerateSlices(MainDir, body)
+        makebodiesvisible()
+        CreateIntersections()
+    End Sub
+
+    Function GetBody() As SurfaceBody
+        ' Have the bodies selected. 
+        Dim baseBody As SurfaceBody
+        baseBody = _invApp.CommandManager.Pick(SelectionFilterEnum.kPartBodyFilter, "Select the base body")
+        Return baseBody
+    End Function
+
+    Function GetBoundingBoxLength(basebody As SurfaceBody) As KeyValuePair(Of Integer, Double)
+        Dim BoundingBox As Box
+        Dim MainDir As Integer
+        BoundingBox = basebody.RangeBox
+
+        Dim Xlength As Double
+        Xlength = BoundingBox.MaxPoint.X - BoundingBox.MinPoint.X
+
+        Dim Ylength As Double
+        Ylength = BoundingBox.MaxPoint.Y - BoundingBox.MinPoint.Y
+
+        Dim Zlength As Double
+        Zlength = BoundingBox.MaxPoint.Z - BoundingBox.MinPoint.Z
+
+        Dim Length As Double
+
+        If XBox.Checked AndAlso YBox.Checked Then
+            _SplitDir = 3
+            If Xlength >= Ylength Then
+                Length = Xlength
+                MainDir = 1
+            Else
+                Length = Ylength
+                MainDir = 2
+            End If
+        ElseIf YBox.Checked AndAlso ZBox.Checked Then
+            _SplitDir = 1
+            If Ylength >= Zlength Then
+                Length = Ylength
+                MainDir = 2
+            Else
+                Length = Zlength
+                MainDir = 3
+            End If
+        Else
+            _SplitDir = 2
+            If Xlength >= Zlength Then
+                Length = Xlength
+                MainDir = 1
+            Else
+                Length = Zlength
+                MainDir = 3
+            End If
+        End If
+
+        Return New KeyValuePair(Of Integer, Double)(MainDir, Length)
+
+    End Function
+
+    Sub GenerateSlices(MainDir As KeyValuePair(Of Integer, Double), baseBody As SurfaceBody)
+
+        Dim basePlane As WorkPlane
+        Dim secondaryPlane As WorkPlane
+        Dim width As Double
+        Dim offset As Double = MainDir.Value / NumberOfSlices.Value
+
+        Dim secondaryOffset As Double
+
+        Dim Spacing As Double
+        Spacing = MainDir.Value / (NumberOfSlices.Value + 1)
+
+        Dim BodyIndex As Integer
+        BodyIndex = _Doc.ComponentDefinition.SurfaceBodies.Count + 1
+
+        Select Case MainDir.Key
+            Case 1
+                basePlane = _Doc.ComponentDefinition.WorkPlanes.AddByPlaneAndOffset(_Doc.ComponentDefinition.WorkPlanes.Item(1), baseBody.RangeBox.MinPoint.X + offset)
+                If YBox.Checked Then
+                    width = baseBody.RangeBox.MaxPoint.Y - baseBody.RangeBox.MinPoint.Y
+                    _SecondarySlicesNumber = Math.Floor(width / Spacing)
+                    secondaryOffset = (width - _SecondarySlicesNumber * Spacing) / 2
+                    secondaryPlane = _Doc.ComponentDefinition.WorkPlanes.AddByPlaneAndOffset(_Doc.ComponentDefinition.WorkPlanes.Item(2), baseBody.RangeBox.MinPoint.Y + secondaryOffset)
+
+                Else
+                    width = baseBody.RangeBox.MaxPoint.Z - baseBody.RangeBox.MinPoint.Z
+                    _SecondarySlicesNumber = Math.Floor(width / Spacing)
+                    secondaryOffset = (width - _SecondarySlicesNumber * Spacing) / 2
+                    secondaryPlane = _Doc.ComponentDefinition.WorkPlanes.AddByPlaneAndOffset(_Doc.ComponentDefinition.WorkPlanes.Item(3), baseBody.RangeBox.MinPoint.Z + secondaryOffset)
+
+                End If
+            Case 2
+                basePlane = _Doc.ComponentDefinition.WorkPlanes.AddByPlaneAndOffset(_Doc.ComponentDefinition.WorkPlanes.Item(2), baseBody.RangeBox.MinPoint.Y + offset)
+                If XBox.Checked Then
+                    width = baseBody.RangeBox.MaxPoint.X - baseBody.RangeBox.MinPoint.X
+                    _SecondarySlicesNumber = Math.Floor(width / Spacing)
+                    secondaryOffset = (width - _SecondarySlicesNumber * Spacing) / 2
+                    secondaryPlane = _Doc.ComponentDefinition.WorkPlanes.AddByPlaneAndOffset(_Doc.ComponentDefinition.WorkPlanes.Item(1), baseBody.RangeBox.MinPoint.X + secondaryOffset)
+
+                Else
+                    width = baseBody.RangeBox.MaxPoint.Z - baseBody.RangeBox.MinPoint.Z
+                    _SecondarySlicesNumber = Math.Floor(width / Spacing)
+                    secondaryOffset = (width - _SecondarySlicesNumber * Spacing) / 2
+                    secondaryPlane = _Doc.ComponentDefinition.WorkPlanes.AddByPlaneAndOffset(_Doc.ComponentDefinition.WorkPlanes.Item(3), baseBody.RangeBox.MinPoint.Z + secondaryOffset)
+
+                End If
+            Case Else
+                basePlane = _Doc.ComponentDefinition.WorkPlanes.AddByPlaneAndOffset(_Doc.ComponentDefinition.WorkPlanes.Item(3), baseBody.RangeBox.MinPoint.Z + offset)
+                If XBox.Checked Then
+                    width = baseBody.RangeBox.MaxPoint.X - baseBody.RangeBox.MinPoint.X
+                    _SecondarySlicesNumber = Math.Floor(width / Spacing)
+                    secondaryOffset = (width - _SecondarySlicesNumber * Spacing) / 2
+                    secondaryPlane = _Doc.ComponentDefinition.WorkPlanes.AddByPlaneAndOffset(_Doc.ComponentDefinition.WorkPlanes.Item(1), baseBody.RangeBox.MinPoint.X + secondaryOffset)
+
+                Else
+                    width = baseBody.RangeBox.MaxPoint.Y - baseBody.RangeBox.MinPoint.Y
+                    _SecondarySlicesNumber = Math.Floor(width / Spacing)
+                    secondaryOffset = (width - _SecondarySlicesNumber * Spacing) / 2
+                    secondaryPlane = _Doc.ComponentDefinition.WorkPlanes.AddByPlaneAndOffset(_Doc.ComponentDefinition.WorkPlanes.Item(2), baseBody.RangeBox.MinPoint.Y + secondaryOffset)
+
+                End If
+        End Select
+
+        basePlane.Name = "Start Puzzle"
+        basePlane.Visible = False
+        secondaryPlane.Visible = False
+
+        CreateSlice(basePlane, baseBody)
+        _Doc.ComponentDefinition.SurfaceBodies.Item(BodyIndex).Name = "P1"
+        _Doc.ComponentDefinition.SurfaceBodies.Item(BodyIndex).Visible = False
+        BodyIndex = BodyIndex + 1
+
+        Dim SliceIndex As Integer
+        Dim SlicePlane As WorkPlane
+
+        For SliceIndex = 1 To (NumberOfSlices.Value - 1)
+            SlicePlane = _Doc.ComponentDefinition.WorkPlanes.AddByPlaneAndOffset(basePlane, SliceIndex * Spacing)
+            SlicePlane.Visible = False
+            CreateSlice(SlicePlane, baseBody)
+            _Doc.ComponentDefinition.SurfaceBodies.Item(BodyIndex).Name = "P" & (SliceIndex + 1)
+            _Doc.ComponentDefinition.SurfaceBodies.Item(BodyIndex).Visible = False
+            BodyIndex = BodyIndex + 1
+        Next
+
+        CreateSlice(secondaryPlane, baseBody)
+        _Doc.ComponentDefinition.SurfaceBodies.Item(BodyIndex).Name = "S1"
+        _Doc.ComponentDefinition.SurfaceBodies.Item(BodyIndex).Visible = False
+        BodyIndex = BodyIndex + 1
+
+        For SliceIndex = 1 To _SecondarySlicesNumber - 1
+            SlicePlane = _Doc.ComponentDefinition.WorkPlanes.AddByPlaneAndOffset(secondaryPlane, SliceIndex * Spacing)
+            SlicePlane.Visible = False
+            CreateSlice(SlicePlane, baseBody)
+            _Doc.ComponentDefinition.SurfaceBodies.Item(BodyIndex).Name = "S" & (SliceIndex + 1)
+            _Doc.ComponentDefinition.SurfaceBodies.Item(BodyIndex).Visible = False
+            BodyIndex = BodyIndex + 1
+        Next
+    End Sub
+
+    Sub CreateSlice(SlicePlane As WorkPlane, Body As SurfaceBody)
+        Dim contoursketch As PlanarSketch = _Doc.ComponentDefinition.Sketches.Add(SlicePlane)
+        Dim ExtrudeThickness = CDbl(SliceThickness.Value)
+        contoursketch.ProjectedCuts.Add()
+
+        Dim exturdeprofiletest As Profile
+        exturdeprofiletest = contoursketch.Profiles.AddForSolid
+        Debug.Print(exturdeprofiletest.Count)
+
+        Dim extrudetest As ExtrudeDefinition
+        extrudetest = _Doc.ComponentDefinition.Features.ExtrudeFeatures.CreateExtrudeDefinition(exturdeprofiletest, PartFeatureOperationEnum.kNewBodyOperation)
+        Call extrudetest.SetDistanceExtent(ExtrudeThickness, PartFeatureExtentDirectionEnum.kSymmetricExtentDirection)
+        _Doc.ComponentDefinition.Features.ExtrudeFeatures.Add(extrudetest)
+
+        Debug.Print(contoursketch.ProjectedCuts.Count)
+    End Sub
+
+    Sub makebodiesvisible()
+        For Each body As SurfaceBody In _Doc.ComponentDefinition.SurfaceBodies
+            body.Visible = True
+        Next
+    End Sub
+
+    Sub CreateIntersections()
+        Dim PrimIndex As Integer
+        Dim SecIndex As Integer
+        Dim BaseBody As SurfaceBody
+        Dim ToolBody As SurfaceBody
+
+        For PrimIndex = 1 To My.Forms.Form1.NumberOfSlices.Value
+            BaseBody = _Doc.ComponentDefinition.SurfaceBodies.Item(PrimIndex + 1)
+            For SecIndex = 1 To _SecondarySlicesNumber
+                ToolBody = _Doc.ComponentDefinition.SurfaceBodies.Item(SecIndex + NumberOfSlices.Value + _initialComp)
+                Try
+                    CreateIntersection(BaseBody, ToolBody)
+                Catch
+
+                End Try
+
+            Next
+        Next
+
+    End Sub
+
+    Sub CreateIntersection(BaseBody As SurfaceBody, ToolBody As SurfaceBody)
+
+        Dim TransBody As TransientBRep = _invApp.TransientBRep
+        Dim transGeo As TransientGeometry = _invApp.TransientGeometry
+        Dim objs As ObjectCollection = _invApp.TransientObjects.CreateObjectCollection
+
+        'Create duplicate bodies
+        Dim transBase As SurfaceBody = TransBody.Copy(BaseBody)
+        Dim transTool As SurfaceBody = TransBody.Copy(ToolBody)
+
+        'Create boolean
+        Call TransBody.DoBoolean(transBase, transTool, BooleanTypeEnum.kBooleanTypeIntersect)
+
+        'Create new solids
+        Dim transdef As NonParametricBaseFeatureDefinition
+        transdef = _Doc.ComponentDefinition.Features.NonParametricBaseFeatures.CreateDefinition()
+        Call objs.Add(transBase)
+        transdef.BRepEntities = objs
+        transdef.OutputType = BaseFeatureOutputTypeEnum.kSolidOutputType
+
+        _Doc.ComponentDefinition.Features.NonParametricBaseFeatures.AddByDefinition(transdef)
+
+        'Create plane
+        Dim LatestBody As SurfaceBody = _CompDef.SurfaceBodies.Item(_CompDef.SurfaceBodies.Count)
+        Dim splitDist As Double
+        Dim WorkPts As WorkPoints = _Doc.ComponentDefinition.WorkPoints
+        Dim WorkPlns As WorkPlanes = _Doc.ComponentDefinition.WorkPlanes
+        Dim splitplane As WorkPlane
+
+        Select Case _SplitDir
+            Case 1
+                splitDist = (LatestBody.RangeBox.MaxPoint.X - LatestBody.RangeBox.MinPoint.X) / 2 + LatestBody.RangeBox.MinPoint.X
+                splitplane = WorkPlns.AddByPlaneAndOffset(_Doc.ComponentDefinition.WorkPlanes.Item(1), splitDist)
+            Case 2
+                splitDist = (LatestBody.RangeBox.MaxPoint.Y - LatestBody.RangeBox.MinPoint.Y) / 2 + LatestBody.RangeBox.MinPoint.Y
+                splitplane = WorkPlns.AddByPlaneAndOffset(_Doc.ComponentDefinition.WorkPlanes.Item(2), splitDist)
+            Case Else
+                splitDist = (LatestBody.RangeBox.MaxPoint.Z - LatestBody.RangeBox.MinPoint.Z) / 2 + LatestBody.RangeBox.MinPoint.Z
+                splitplane = WorkPlns.AddByPlaneAndOffset(_Doc.ComponentDefinition.WorkPlanes.Item(3), splitDist)
+        End Select
+
+        splitplane.Visible = False
+
+        'Split
+        Dim splitfeat As SplitFeature = _Doc.ComponentDefinition.Features.SplitFeatures.SplitBody(splitplane, LatestBody)
+
+        'Scale
+        'Dim DirectFs As DirectEditFeatures = _Doc.ComponentDefinition.Features.DirectEditFeatures
+        'Dim Scale As DirectEditScaleOperation = 
+
+
+        'Bool
+        Dim CombineColl1 As ObjectCollection = _invApp.TransientObjects.CreateObjectCollection
+        CombineColl1.Add(_CompDef.SurfaceBodies.Item(_CompDef.SurfaceBodies.Count))
+
+        Dim combo1 As CombineFeature = _Doc.ComponentDefinition.Features.CombineFeatures.Add(ToolBody, CombineColl1, PartFeatureOperationEnum.kCutOperation, False)
+
+        Dim CombineColl2 As ObjectCollection = _invApp.TransientObjects.CreateObjectCollection
+        CombineColl2.Add(_CompDef.SurfaceBodies.Item(_CompDef.SurfaceBodies.Count))
+        Dim combo2 As CombineFeature = _Doc.ComponentDefinition.Features.CombineFeatures.Add(BaseBody, CombineColl2, PartFeatureOperationEnum.kCutOperation, False)
+
+    End Sub
+
+    Private Sub Xdir_CheckedChanged(sender As Object, e As EventArgs)
+        If Xdir.Checked Then
+            Ydir.Checked = False
+            Zdir.Checked = False
+            _PullDir = 1
+        End If
+    End Sub
+
+    Private Sub Ydir_CheckedChanged(sender As Object, e As EventArgs)
+        If Ydir.Checked Then
+            Xdir.Checked = False
+            Zdir.Checked = False
+            _PullDir = 2
+        End If
+    End Sub
+
+    Private Sub Zdir_CheckedChanged(sender As Object, e As EventArgs)
+        If Zdir.Checked Then
+            Xdir.Checked = False
+            Ydir.Checked = False
+            _PullDir = 3
+        End If
+    End Sub
+
 
 End Class
+
